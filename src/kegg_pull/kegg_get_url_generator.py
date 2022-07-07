cce4351a7a2064416c82687a0f81936cda4380f5
erikhuck <edhu227@poseidon.cesb.uky.edu>
2022-07-07 15:52:28 -0400

    Implements the KEGGgetURLgenerator

--- src/kegg_pull/kegg_get_url_generator.py
+++ src/kegg_pull/kegg_get_url_generator.py
@@ -1,6 +1,7 @@
 import logging
+from requests import Response
 
-from src.kegg_pull.kegg_url import GetKEGGurl
+from src.kegg_pull.kegg_url import ListKEGGurl, GetKEGGurl
 from src.kegg_pull.pull_single_from_kegg import pull_single_from_kegg
 
 MAX_KEGG_ENTRY_IDS_PER_GET_URL: int = 10
@@ -17,7 +18,12 @@ class KEGGgetURLgenerator:
 
         self._entry_field = entry_field
 
-        # TODO: Complete implementation
+        if database_type is not None:
+            self._entry_id_list: list = self._get_entry_id_list_from_kegg_list_api_operation(
+                database_type=database_type
+            )
+        else:
+            self._entry_id_list: list = self._get_entry_id_list_from_file(entry_id_list_path=entry_id_list_path)
 
     @staticmethod
     def _validate(database_type: str, entry_id_list_path: str):
@@ -31,3 +37,37 @@ class KEGGgetURLgenerator:
                 'Required: Either a file containing a list of KEGG entry IDs or the name of a KEGG database from which '
                 'the entry IDs can be pulled. Neither are provided'
             )
+
+    @staticmethod
+    def _get_entry_id_list_from_kegg_list_api_operation(database_type: str) -> list:
+        list_url = ListKEGGurl(database_type=database_type)
+        res: Response = pull_single_from_kegg(kegg_url=list_url)
+        entry_ids: list = KEGGgetURLgenerator._parse_entry_ids_string(entry_ids_string=res.text)
+
+        # We empirically determined that each line consists of the entry ID followed by more info separated by a tab
+        for i, entry_id in enumerate(entry_ids):
+            entry_id: str = entry_id.split('\t')[0]
+            entry_ids[i] = entry_id
+
+        return entry_ids
+
+    @staticmethod
+    def _parse_entry_ids_string(entry_ids_string: str) -> list:
+        entry_ids: list = entry_ids_string.strip().split('\n')
+
+        return entry_ids
+
+    @staticmethod
+    def _get_entry_id_list_from_file(entry_id_list_path: str) -> list:
+        with open(entry_id_list_path, 'r') as f:
+            entry_ids: str = f.read()
+            entry_ids: list = KEGGgetURLgenerator._parse_entry_ids_string(entry_ids_string=entry_ids)
+
+        return entry_ids
+
+    def __iter__(self):
+        for i in range(0, len(self._entry_id_list), self._n_entries_per_url):
+            entry_ids: list = self._entry_id_list[i:i+self._n_entries_per_url]
+            get_kegg_url = GetKEGGurl(entry_ids=entry_ids, entry_field=self._entry_field)
+
+            yield get_kegg_url
--- tests/test_kegg_get_url_generator.py
+++ tests/test_kegg_get_url_generator.py
@@ -1,11 +1,23 @@
 from pytest import raises
 
+from src.kegg_pull.kegg_url import BASE_URL
 from src.kegg_pull.kegg_get_url_generator import KEGGgetURLgenerator
 from tests.utils import assert_expected_error_message, assert_warning
 
 
-def test_validate(caplog):
-    KEGGgetURLgenerator(database_type='', entry_id_list_path='')
+def test_validate(caplog, mocker):
+    mock_get_entry_id_list_from_kegg_list_api_operation = mocker.patch(
+        'src.kegg_pull.kegg_get_url_generator.KEGGgetURLgenerator._get_entry_id_list_from_kegg_list_api_operation'
+    )
+
+    mock_get_entry_id_list_from_file = mocker.patch(
+        'src.kegg_pull.kegg_get_url_generator.KEGGgetURLgenerator._get_entry_id_list_from_file'
+    )
+
+    database_type = 'vg'
+    KEGGgetURLgenerator(database_type=database_type, entry_id_list_path='')
+    mock_get_entry_id_list_from_kegg_list_api_operation.assert_called_once_with(database_type=database_type)
+    mock_get_entry_id_list_from_file.assert_not_called()
 
     assert_warning(
         file_name='kegg_get_url_generator.py', func_name='_validate',
@@ -20,3 +32,42 @@ def test_validate(caplog):
         expected_message='Required: Either a file containing a list of KEGG entry IDs or the name of a KEGG database '
                          'from which the entry IDs can be pulled. Neither are provided', e=e
     )
+
+
+# TODO: Test with and without entry field
+# TODO: Test with entry field that can pull multiple and entry field that only pulls single
+# TODO: Test getting from file (mock open)
+def test_iter(mocker):
+    mock_response_body = '''
+br:br08901	KEGG pathway maps
+br:br08902	BRITE hierarchy files
+br:br08904	BRITE table files
+br:ko00001	KEGG Orthology (KO)
+br:ko00002	KEGG modules
+br:ko00003	KEGG reaction modules
+br:br08907	KEGG networks
+br:ko01000	Enzymes
+br:ko01001	Protein kinases
+br:ko01009	Protein phosphatases and associated proteins
+br:ko01002	Peptidases and inhibitors
+br:ko01003	Glycosyltransferases
+    '''
+
+    expected_urls = [
+        f'{BASE_URL}/get/br:br08901+br:br08902+br:br08904+br:ko00001+br:ko00002+br:ko00003+br:br08907+br:ko01000+'
+        'br:ko01001+br:ko01009',
+        f'{BASE_URL}/get/br:ko01002+br:ko01003'
+    ]
+
+    mock_pull_single_from_kegg_return_value = mocker.MagicMock(text=mock_response_body)
+
+    mock_pull_single_from_kegg = mocker.patch(
+        'src.kegg_pull.kegg_get_url_generator.pull_single_from_kegg',
+        return_value=mock_pull_single_from_kegg_return_value
+    )
+
+    kegg_get_url_generator = KEGGgetURLgenerator(database_type='brite')
+    mock_pull_single_from_kegg.assert_called_once()
+
+    for get_kegg_url, expected_url in zip(kegg_get_url_generator, expected_urls):
+        assert get_kegg_url.url == expected_url
