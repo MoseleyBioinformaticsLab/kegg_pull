d4446f0a3cfde756d8c6328e768510f5285aec85
erikhuck <erik.huckvale2017@gmail.com>
2022-09-01 13:12:56 -0400

    Sets up automatic documentation generation from doc strings

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,4 @@ import kegg_pull.kegg_url as ku
 def mock_organism_set(mocker, request):
     if not 'disable_mock_organism_set' in request.keywords:
         organism_set_mock = {'organism-code', 'organism-T-number'}
-
-        mocker.patch.object(
-            ku.AbstractKEGGurl, 'organism_set', organism_set_mock
-        )
+        mocker.patch('kegg_pull.kegg_url.AbstractKEGGurl.organism_set', return_value=organism_set_mock)
