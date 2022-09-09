f141afc63b450537ebf7579d48c528e8c482d355
erikhuck <erik.huckvale2017@gmail.com>
2022-09-09 10:38:44 -0400

    Completes documentation of the EntryIdsGetter class

--- src/kegg_pull/entry_ids.py
+++ src/kegg_pull/entry_ids.py
@@ -12,10 +12,20 @@ class EntryIdsGetter:
     """
     Class with methods for various ways of getting a list of KEGG entry IDs from the KEGG REST API.
     """
-    def __init__(self, kegg_rest: r.KEGGrest = None):
+
+    def __init__(self, kegg_rest: r.KEGGrest = None) -> None:
+        """
+        :param kegg_rest: Optional KEGGrest object for making requests to the KEGG REST API to get entry IDs (if a KEGGrest object is not provided, a KEGGrest object is created with the default values).
+        """
         self._kegg_rest = kegg_rest if kegg_rest is not None else r.KEGGrest()
 
     def from_database(self, database_name: str) -> list:
+        """ Gets the KEGG entry IDs of a given database.
+
+        :param database_name: The KEGG database to get the entry IDs for.
+        :return: The list of resulting entry IDs.
+        :raises RuntimeError: Raised if the request to the KEGG REST API fails or times out.
+        """
         kegg_response: r.KEGGresponse = self._kegg_rest.list(database_name=database_name)
 
         return EntryIdsGetter._process_response(kegg_response=kegg_response)
@@ -23,6 +33,12 @@ class EntryIdsGetter:
 
     @staticmethod
     def _process_response(kegg_response: r.KEGGresponse) -> list:
+        """ Extracts the entry IDs from a KEGG response if successful, else raises an exception.
+
+        :param kegg_response: The response from the KEGG REST API containing entry IDs if successful.
+        :return: The list of KEGG entry IDs.
+        :raises RuntimeError: Raised if the KEGG response indicates a failure or time out.
+        """
         if kegg_response.status == r.KEGGresponse.Status.FAILED:
             raise RuntimeError(
                 f'The KEGG request failed to get the entry IDs from the following URL: {kegg_response.kegg_url.url}'
@@ -40,6 +56,12 @@ class EntryIdsGetter:
 
     @staticmethod
     def from_file(file_path: str) -> list:
+        """ Gets KEGG entry IDs that are listed in a file with one entry ID on each line.
+
+        :param file_path: The path to the file containing the entry IDs.
+        :return: The list of entry IDs.
+        :raises ValueError: Raised if the file is empty.
+        """
         with open(file_path, 'r') as file:
             entry_ids: str = file.read()
 
@@ -53,6 +75,11 @@ class EntryIdsGetter:
 
     @staticmethod
     def _parse_entry_ids_string(entry_ids_string: str) -> list:
+        """ Parses the entry IDs contained in a string.
+
+        :param entry_ids_string: The string containing the entry IDs.
+        :return: The list of parsed entry IDs.
+        """
         entry_ids: list = entry_ids_string.strip().split('\n')
         entry_ids = [entry_id.split('\t')[0].strip() for entry_id in entry_ids]
 
@@ -60,6 +87,13 @@ class EntryIdsGetter:
 
 
     def from_keywords(self, database_name: str, keywords: list) -> list:
+        """ Gets entry IDs from a database based on keywords searched in the entries.
+
+        :param database_name: The name of the database to get entry IDs from.
+        :param keywords: The keywords to search entries in the database with.
+        :return: The list of entry IDs.
+        :raises RuntimeError: Raised if the request to the KEGG REST API fails or times out.
+        """
         kegg_response: r.KEGGresponse = self._kegg_rest.keywords_find(database_name=database_name, keywords=keywords)
 
         return EntryIdsGetter._process_response(kegg_response=kegg_response)
@@ -69,6 +103,15 @@ class EntryIdsGetter:
         self, database_name: str, formula: str = None, exact_mass: t.Union[float, tuple] = None,
         molecular_weight: t.Union[int, tuple] = None
     ) -> list:
+        """ Gets entry IDs from a database containing chemical entries based on one (and only one) of three molecular attributes of the entries.
+
+        :param database_name: The name of the database containing chemical entries.
+        :param formula: The chemical formula to search for.
+        :param exact_mass: The exact mass of the compound to search for (a single value or a range).
+        :param molecular_weight: The molecular weight of the compound to search for (a single value or a range).
+        :return: The list of entry IDs.
+        :raises RuntimeError: Raised if the request to the KEGG REST API fails or times out.
+        """
         kegg_response: r.KEGGresponse = self._kegg_rest.molecular_find(
             database_name=database_name, formula=formula, exact_mass=exact_mass, molecular_weight=molecular_weight
         )
