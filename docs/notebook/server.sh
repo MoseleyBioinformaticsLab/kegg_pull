592cb478f6c90b0c722fd70a203af6826bdea18e
erikhuck <erikhuck@users.noreply.github.com>
2022-09-17 00:17:23 +0000

    Update documentation.

--- docs/notebook/server.sh
+++
@@ -1,4 +0,0 @@
-# This will start the server and print the URL that you need to copy and paste into your web browser.
-# Note that if you're port forwarding, you need to replace the port number in the URL with the one that you're forwarding to localhost (your local computer) rather than the one you're running on a lab machine.
-jupyter notebook
-
--- docs/notebook/to-rst.sh
+++
@@ -1,4 +0,0 @@
-# Note this will only work if pandoc is installed separately via "sudo dnf install pandoc"
-jupyter nbconvert --to rst tutorial.ipynb
-mv tutorial.rst ../tutorial.rst
-
