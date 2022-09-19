5535bca2da6ec02da5531a51ff8f0f48af18174c
erikhuck <erikhuck@users.noreply.github.com>
2022-09-19 00:47:12 +0000

    Update documentation.

--- dev/install.sh
+++
@@ -1,8 +0,0 @@
-echo "Removing previous .env/ directory if it exists..."
-rm -rf .env/
-echo "Creating new .env/ directory..."
-python3 -m venv .env/
-source .env/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install pytest pytest-mock pytest-cov sphinx sphinx-rtd-theme notebook
-python3 -m pip install -e .
--- dev/test.sh
+++
@@ -1,3 +0,0 @@
-source .env/bin/activate
-
-python -m pytest dev --cov --cov-branch --cov-report=term-missing
