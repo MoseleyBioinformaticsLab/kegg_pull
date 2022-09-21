dce6c1c02be1f10816090aba5f349d4a681ec496
erikhuck <erik.huckvale2017@gmail.com>
2022-09-21 10:51:57 -0400

    Sets up repo for publishing to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools as st
+import src.kegg_pull as kp
 
 
 def _get_requirements() -> list:
@@ -11,7 +12,20 @@ def _get_requirements() -> list:
     return requirements
 
 
+def _readme():
+    with open('README.rst') as readme_file:
+        return readme_file.read()
+
+
 st.setup(
-    name='kegg_pull', package_dir={'': 'src'}, install_requires=_get_requirements(),
-    entry_points={"console_scripts": ["kegg_pull = kegg_pull.__main__:main"]}
+    name='kegg_pull',
+    version=kp.__version__,
+    package_dir={'': 'src'},
+    install_requires=_get_requirements(),
+    entry_points={"console_scripts": ["kegg_pull = kegg_pull.__main__:main"]},
+    author='Erik Huckvale',
+    author_email='edhu227@g.uky.edu',
+    url='https://github.com/MoseleyBioinformaticsLab/KEGGpull',
+    long_description_content_type="text/x-rst",
+    long_description=_readme()
 )
