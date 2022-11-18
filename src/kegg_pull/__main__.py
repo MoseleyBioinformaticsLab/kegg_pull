230807468acce07ea9a1eb89271d7a50d8053e0c
erikhuck <erik.huckvale2017@gmail.com>
2022-11-18 15:14:27 -0500

    Makes top-level CLI output more consistent with documentation and simplifies __main__.py

--- src/kegg_pull/__main__.py
+++ src/kegg_pull/__main__.py
@@ -3,35 +3,37 @@ Usage:
     kegg_pull -h | --help       Show this help message.
     kegg_pull -v | --version    Displays the package version.
     kegg_pull --full-help       Show the help message of all sub commands.
+    kegg_pull pull ...          Pull, separate, and store an arbitrary number of KEGG entries to the local file system.
     kegg_pull entry-ids ...     Obtain a list of KEGG entry IDs.
     kegg_pull rest ...          Executes one of the KEGG REST API operations.
-    kegg_pull pull ...          Pull, separate, and store KEGG entries to the local file system.
 """
 import sys
 
 from . import __version__
+from . import pull_cli as p_cli
 from . import entry_ids_cli as ei_cli
 from . import rest_cli as r_cli
-from . import pull_cli as p_cli
 
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1] == 'entry-ids':
-        ei_cli.main()
-    elif len(sys.argv) > 1 and sys.argv[1] == 'rest':
-        r_cli.main()
-    elif len(sys.argv) > 1 and sys.argv[1] == 'pull':
+    first_arg: str = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if first_arg == 'pull':
         p_cli.main()
-    elif len(sys.argv) > 1 and sys.argv[1] == '--full-help':
+    elif first_arg == 'entry-ids':
+        ei_cli.main()
+    elif first_arg == 'rest':
+        r_cli.main()
+    elif first_arg == '--full-help':
         separator = '-'*80
         print(__doc__)
+        print(separator)
+        print(p_cli.__doc__)
         print(separator)
         print(ei_cli.__doc__)
         print(separator)
         print(r_cli.__doc__)
-        print(separator)
-        print(p_cli.__doc__)
-    elif len(sys.argv) > 1 and (sys.argv[1] == '--version' or sys.argv[1] == '-v'):
+    elif first_arg == '--version' or first_arg == '-v':
         print(__version__)
     else:
         print(__doc__)
